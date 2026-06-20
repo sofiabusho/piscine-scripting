f05dcb0a432bdba12376dc8b57db46ef392a961b
sbusho <sofiaabusho@gmail.com>
2026-06-21 00:36:36 +0300

    change permissions

--- hard-perm.sh
+++ hard-perm.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-chmod u-wx,g-rwx,o-r hard-perm/0
-chmod u-w,g-rw,o-r+w hard-perm/1
-chmod g-rw hard-perm/2
-chmod o+w hard-perm/3
-chmod u-w+x,g-rw+x,o-r hard-perm/4
-chmod u-w,o-r hard-perm/5
-chmod u-w,o-r hard-perm/6
-chmod u-w+x,g-rw+x,o-r hard-perm/7
-chmod g-rw hard-perm/8
-chmod u-w,g-rw,o-r+w hard-perm/9
-chmod u-wx,g-rwx,o-r hard-perm/A  
+chmod 401 hard-perm/0
+chmod 402 hard-perm/1
+chmod 601 hard-perm/2
+chmod 777 hard-perm/3
+chmod 510 hard-perm/4
+chmod 460 hard-perm/5
+chmod 460 hard-perm/6
+chmod 510 hard-perm/7
+chmod 604 hard-perm/8
+chmod 402 hard-perm/9
+chmod 401 hard-perm/A  
