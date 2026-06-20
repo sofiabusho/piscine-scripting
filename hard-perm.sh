4800672a340b73ac96129d587e0bf272f51d02ad
sbusho <sofiaabusho@gmail.com>
2026-06-21 00:28:59 +0300

    change permissions

--- hard-perm.sh
+++ hard-perm.sh
@@ -2,7 +2,7 @@
 chmod u-wx,g-rwx,o-r hard-perm/0
 chmod u-w,g-rw,o-r+w hard-perm/1
 chmod g-rw hard-perm/2
-chmod o-w hard-perm/3
+chmod o+w hard-perm/3
 chmod u-w+x,g-rw+x,o-r hard-perm/4
 chmod u-w,o-r hard-perm/5
 chmod u-w,o-r hard-perm/6
