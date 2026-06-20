fe95d0ee1bd26c6a9c0d9e99e25975e6c2ed51bb
sbusho <sofiaabusho@gmail.com>
2026-06-21 00:58:32 +0300

    show specific details

--- file-details.sh
+++ file-details.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ls -l --time-style=long-iso hard-perm/ | sed '/^total /d' | awk '{print $1, $6, $7, $8}'  
+ls -l --time-style=long-iso | sed '/^total /d' | awk '{print $1, $6, $7, $8}'  
