90a23782041881095d6a0fc6d343c5c3dfb51bb0
sbusho <sofiaabusho@gmail.com>
2026-06-21 01:46:02 +0300

    internal variabless

--- set-internal-vars.sh
+++ set-internal-vars.sh
@@ -4,4 +4,4 @@ MY_NUM=100
 MY_PI=3.142
 MY_ARR="one two three four five"
 
-printf '%s\n%s\n%s\n%s\n' "$MY_MESSAGE" "$MY_NUM" "$MY_PI" "$(echo "$MY_ARR" | sed 's/ /, /g')"
+printf '%s\n%s\n%s\n%s\n' "$MY_MESSAGE" "$MY_NUM" "$MY_PI" "$(echo "${MY_ARR// /, }" | sed 's/ /, /g')"
