6b2d79f94501cb90ef2d0c750533b728be7b50b6
sbusho <sofiaabusho@gmail.com>
2026-06-23 04:19:30 +0300

    compare

--- comparator.sh
+++ comparator.sh
@@ -10,9 +10,9 @@ if ! [[ $1 =~ ^[0-9]+$ ]]; then
 fi
 
 if [[ $1 -gt $2 ]]; then
-    echo "$1 < $2"
+    echo "$1 > $2"
 elif [[ $1 -lt $2 ]]; then
-    echo "$2 > $1"
+    echo "$1 < $2"
 elif [[ $1 -eq $2 ]]; then
     echo "$1 = $2"
 fi
