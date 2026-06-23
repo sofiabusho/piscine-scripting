19896429871f04439072d94e8d7fce24f9a27b21
sbusho <sofiaabusho@gmail.com>
2026-06-23 04:23:19 +0300

    compare

--- comparator.sh
+++ comparator.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [[ $# -ne 2 ]]; then
     echo "Error: The script only works with two arguments!"
-    exit 1
+    exit 0
 fi
 
 if ! [[ $1 =~ ^-?[0-9]+$ ]]; then
     echo "Error: Only two numeric arguments are acceptable!"
-    exit 1
+    exit 0
 fi
 
 if [[ $1 -gt $2 ]]; then
