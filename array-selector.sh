799ee17c30920b24f6b50268d0b6efd286e52acc
sbusho <sofiaabusho@gmail.com>
2026-06-23 03:54:35 +0300

    colors

--- array-selector.sh
+++ array-selector.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-array=("red" "blue" "green" "white" "black")
+color=("red" "blue" "green" "white" "black")
 
 if [[ $# -ne 1 ]]; then
     echo "Error"
@@ -15,5 +15,15 @@ if [[ "$1" -lt 1 ]] || [[ "$1" -gt 5 ]]; then
     exit 1
 fi
 
-index=$(( $1 - 1 ))
-echo "${array[$index]}"
+
+if [[ "$1" -eq 1 ]]; then
+    echo ${color[0]}
+elif [[ "$1" -eq 2 ]]; then
+    echo ${color[1]}
+elif [[ "$1" -eq 3 ]]; then
+    echo ${color[2]}
+elif [[ "$1" -eq 4 ]]; then
+    echo ${color[3]}
+elif [[ "$1" -eq 5 ]]; then
+    echo ${color[4]}
+fi
