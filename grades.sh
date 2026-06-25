ce4927431cc6efda5b5c32407f652cd76f12f143
sbusho <sofiaabusho@gmail.com>
2026-06-26 02:08:25 +0300

    grades

--- grades.sh
+++ grades.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
-student=$1
-
 if [[ $# -ne 1 ]]; then
-    echo "Error: expect 1 argument only!"
+    >&2 echo "Error: expect 1 argument only!"
     exit 1
 fi
 
-echo "Student Name #$student: "
-        read name
-echo "Student Grade #$student: "
-        read grade
-        if ! [[ "$grade" =~ ^[0-9]+$ ]] || [[ $grade -lt 1 ]] || [[ $grade -gt 100 ]]; then
-            echo "Error: The grade $grade is not a valid input. Only numerical grades between 0 and 100 are accepted."
-            exit 1
-        fi
-        if [[ "$grade" -gt 90 ]]; then
-            echo "$name: You did an excellent job!"
-        elif [[ "$grade" -lt 90 ]] && [[ "$grade" -gt 70 ]]; then
-            echo "$name: You did a good job!"
-        elif [[ "$grade" -lt 70 ]] && [[ "$grade" -gt 50 ]]; then
-            echo "$name: You need a bit more effort!"
-        elif [[ "$grade" -lt 50 ]]; then
-            echo "$name: You had a poor performance!"
-        fi
+declare -a names
+declare -a grades
+
+for ((i = 1; i <= $1; i++)); do
+    read -p "Student Name #$i: " name
+    read -p "Student Grade #$i: " grade
+    if ! [[ "$grade" =~ ^[0-9]+$ ]] || [[ $grade -lt 0 ]] || [[ $grade -gt 100 ]]; then
+        >&2 echo "Error: The grade '$grade' is not a valid input. Only numerical grades between 0 and 100 are accepted."
+        exit 1
+    fi
+    names+=("$name")
+    grades+=("$grade")
+done
+
+for ((i = 0; i < $1; i++)); do
+    name="${names[$i]}"
+    grade="${grades[$i]}"
+    if [[ $grade -ge 90 ]]; then
+        echo "$name: You did an excellent job!"
+    elif [[ $grade -ge 70 ]]; then
+        echo "$name: You did a good job!"
+    elif [[ $grade -ge 50 ]]; then
+        echo "$name: You need a bit more effort!"
+    else
+        echo "$name: You had a poor performance!"
+    fi
+done
